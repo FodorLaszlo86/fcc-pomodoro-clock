2a8d038f6826fc1daf79132a37da327657711417
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-01 22:39:06 +0100

    ids were added to respective jsx element

--- src/components/DurationControls.js
+++ src/components/DurationControls.js
@@ -4,19 +4,19 @@ const DurationControls = ({ sessionDuration, breakDuration, incrementTime, decre
     return (
         <div>
             <div>
-                <h3>Session Length</h3>
+                <h3 id='session-label'>Session Length</h3>
                 <div>
-                    <div id='decrement-session' onClick={ decrementTime }>&#8722;</div>
-                    <div>{ sessionDuration }</div>
-                    <div id='increment-session' onClick={ incrementTime }>&#43;</div>
+                    <div id='session-decrement' onClick={ decrementTime }>&#8722;</div>
+                    <div id='session-length'>{ sessionDuration }</div>
+                    <div id='session-increment' onClick={ incrementTime }>&#43;</div>
                 </div>
             </div>
             <div>
-                <h3>Break Length</h3>
+                <h3 id='break-label'>Break Length</h3>
                 <div>
-                    <div id='decrement-break' onClick={ decrementTime }>&#8722;</div>
-                    <div>{ breakDuration }</div>
-                    <div id='increment-break' onClick={ incrementTime }>&#43;</div>
+                    <div id='break-decrement' onClick={ decrementTime }>&#8722;</div>
+                    <div id='break-length'>{ breakDuration }</div>
+                    <div id='break-increment' onClick={ incrementTime }>&#43;</div>
                 </div>
             </div>
         </div>

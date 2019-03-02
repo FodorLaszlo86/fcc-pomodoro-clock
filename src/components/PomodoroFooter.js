10ebae2cf2d5d097cd84bf61e9938df0befc04a0
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-02 23:13:44 +0100

    app header and footer components added

---
+++ src/components/PomodoroFooter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PomodoroFooter = () => {
+    return (
+        <footer id='footer'>Coded by <span>Laszlo Fodor</span></footer>
+    )
+}
+
+export default PomodoroFooter;
---
+++ src/components/PomodoroHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PomodoroHeader = () => {
+    return (
+        <header>
+            <h1 id='title'>FCC Pomodoro Clock</h1>
+        </header>
+    )
+}
+
+export default PomodoroHeader;

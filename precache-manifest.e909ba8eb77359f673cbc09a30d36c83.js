9707e89378e60a51ac5e0b45f210abab453d2f3f
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-03 15:50:50 +0100

    Updates

---
+++ precache-manifest.e909ba8eb77359f673cbc09a30d36c83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebdaeae731c67a898e06a5a9e5400d19",
+    "url": "/fcc-pomodoro-clock/static/media/pomodoro_background.ebdaeae7.jpg"
+  },
+  {
+    "revision": "861edab1e102bec14060",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.861edab1.js"
+  },
+  {
+    "revision": "766d547edf0e1813c682",
+    "url": "/fcc-pomodoro-clock/static/js/main.766d547e.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/fcc-pomodoro-clock/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "766d547edf0e1813c682",
+    "url": "/fcc-pomodoro-clock/static/css/main.5301e17f.chunk.css"
+  },
+  {
+    "revision": "d93d0e8b23d2c812d160174ac7ae70cc",
+    "url": "/fcc-pomodoro-clock/index.html"
+  }
+];
